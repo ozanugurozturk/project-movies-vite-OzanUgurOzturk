7d8475770699b54f6ff0ef1d4e89cd875f25de25
ouozturk <ozan.ugur.ozturk@student.forsbergsskola.se>
2023-11-11 23:03:13 +0100

    Naming issues are fixed

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -21,7 +21,7 @@ const MovieList = () => {
             {movies.map((movie) => (
                 <Link key={movie.id} to={`/movies/${movie.id}`}>
                     <img src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`} alt={movie.title} />
-                    <div className="details">
+                    <div className="info">
                         <h1>{movie.title}</h1>
                         <p>Released {movie.release_date}</p>
                     </div>
