1e8921bbf91b09c172e3a17bb0f0d2ba5dcfc9af
ouozturk <ozan.ugur.ozturk@student.forsbergsskola.se>
2023-11-13 03:52:21 +0100

    Revert "Links are created within Details"
    This reverts commit 7ecdf8ad7734dc7abd0a053e6e36adbb082d83c1.

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -6,16 +6,15 @@ import CollectionDetails from "./CollectionDetails";
 import GenreDetails from "./GenreDetails";
 import CompanyDetails from "./CompanyDetails";
 
-
 const Routes = ({ apiKey }) => {
     return (
         <Router>
             <ReactRoutes>
                 <Route path="/" element={<MovieList apiKey={apiKey} />} />
                 <Route path="/movies/:id" element={<MovieDetail apiKey={apiKey} />} />
-                <Route path="/collections/:id" component={CollectionDetails} apiKey={apiKey} />
-                <Route path="/genres/:id" component={GenreDetails} apiKey={apiKey} />
-                <Route path="/companies/:id" component={CompanyDetails} apiKey={apiKey} />
+                <Route path="/collections/:id" element={<CollectionDetails />} />
+                <Route path="/genres/:id" element={<GenreDetails />} />
+                <Route path="/companies/:id" element={<CompanyDetails />} />
             </ReactRoutes>
         </Router>
     );
