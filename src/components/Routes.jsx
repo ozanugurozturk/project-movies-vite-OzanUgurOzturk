37d318aa7dc06cc5df8257ec276baeeb2e8f69cc
ouozturk <ozan.ugur.ozturk@student.forsbergsskola.se>
2023-11-13 01:55:32 +0100

    new endpoints are initiated

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes as ReactRoutes } from 'react-router-dom';
 import MovieList from "./MovieList";
 import MovieDetail from "./MovieDetail";
+import CollectionDetails from "./CollectionDetails";
+import GenreDetails from "./GenreDetails";
+import CompanyDetails from "./CompanyDetails";
 
 const Routes = ({ apiKey }) => {
     return (
@@ -9,6 +12,9 @@ const Routes = ({ apiKey }) => {
             <ReactRoutes>
                 <Route path="/" element={<MovieList apiKey={apiKey} />} />
                 <Route path="/movies/:id" element={<MovieDetail apiKey={apiKey} />} />
+                <Route path="/collections/:id" element={<CollectionDetails />} />
+                <Route path="/genres/:id" element={<GenreDetails />} />
+                <Route path="/companies/:id" element={<CompanyDetails />} />
             </ReactRoutes>
         </Router>
     );
